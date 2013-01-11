5563f575a4ca0308499b8d582a8e26146a0ac644
Marcos Javier Riganti <marcos.riganti@gmail.com>
2013-01-11 15:30:41 -0800

    adding responsive to the dialogs

--- app/scripts/mjr-gallery-dialog/mjr-gallery-dialog.js
+++ app/scripts/mjr-gallery-dialog/mjr-gallery-dialog.js
@@ -6,12 +6,14 @@ var Dialog = Y.Base.create(
     [],     // class extensions
     {
         initializer: function(){
-            console.log('initializer');
+            // console.log('initializer');
             self = this;
-
-            Y.after('resize',function(){
-                self.fire(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION);
-            });
+            
+            Y.on('resize',function(evt){
+                self.fire(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION,self);
+            },null,self);
+            
+            self.on(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION,self.updatePosition);
 
         },
         bindUI: function(){
@@ -23,42 +25,53 @@ var Dialog = Y.Base.create(
                     self.fire(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION);
                 },10);
             });
-
             self._renderUpdate(null);
             
-
-            self.after(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION,self.updatePosition,self);
-
         },
         syncUI: function(){
             this.fire(Y.namespace('MJR.GALLERY').Dialog.EV_UPDATE_POSITION);
         },
         updatePosition: function(){
+            
+
             var self   = this,
                 center = self.get('center'),
                 panel  = self.get('boundingBox'),
                 wHeight = panel.get('winHeight'),
                 wWidth = panel.get('winWidth');
 
+            // console.log('calling updatePosition for:'+panel.get('id')+'-'+self.get('visible'));
+
             if (!self.get('visible')) return;
 
-            var 
-                left = Math.floor((wWidth -  this.get('width')) / 2),
-                top  = Math.max(0,Math.floor((wHeight -  parseInt(panel.getComputedStyle('height'),10)  ) / 2));
+            var left = Math.floor((wWidth -  this.get('width')) / 2),
+                top  = Math.floor((wHeight -  parseInt(panel.getComputedStyle('height'),10)  ) / 2);
 
             if(center){
-                console.log(top);
                 panel.setStyles({
-                    'left': left,
-                    'top': top
+                    'left': Math.max(0,left),
+                    'top': Math.max(0,top)
                 });
+            }
+            if(top < 0 ){
+                self.set('height',wHeight);
+                panel.addClass(self.getClassName('overflow'));
+            }else{
+                if(panel.hasClass(self.getClassName('overflow'))){
+                    panel.setStyle('height',null);
+                    self.set('height',null)
+                    panel.one('.yui3-widget-bd').setStyle('height',null);
+                    panel.removeClass(self.getClassName('overflow'));
+                    self.updatePosition();
+                    return;
+                }  
             }
         },
         _renderUpdate: function(evt){
             var self  = this,
                 panel = self.get('boundingBox'),
-                hd    = panel.one('.yui3-widget-hd').generateID(),
-                bd    = panel.one('.yui3-widget-bd').generateID();
+                hd    = panel.all('.yui3-widget-hd').generateID(),
+                bd    = panel.all('.yui3-widget-bd').generateID();
 
                 panel
                   .setAttribute('role','dialog')
